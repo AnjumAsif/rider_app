4adc35e6e22c8168b9c1c7d8f926921989854476
Asif Anjum <asifanjum.bca008@gmail.com>
2021-04-13 01:10:33 +0530

    login with progressdoen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,35 +2,53 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:rider_app/main.dart';
 import 'package:rider_app/screens/main_screen.dart';
 import 'package:rider_app/screens/registration_screen.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   static const String idScreen = "loginScreen";
-  String emailController ;
-  String passwordController ;
 
-  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  String emailController ;
+
+  String passwordController ;
+  bool showSpinner = false;
+
+  final _firebaseAuth = FirebaseAuth.instance;
 
   void loginAuthenticateUser(BuildContext context) async {
-    final userCredential = await _firebaseAuth
-        .signInWithEmailAndPassword(
-            email: emailController, password: passwordController)
-        .catchError((onError) {
-      displayMessage("Error: " + onError.toString());
-    });
 
-    final User userData = userCredential.user;
-    if (userData != null) {
-      userRef.child(userData.uid).once().then((value) => (DataSnapshot snap) {
+    final user = (await _firebaseAuth.signInWithEmailAndPassword(
+        email: emailController, password: passwordController).catchError((onError) {
+          setState(() {
+            showSpinner=false;
+          });
+      displayMessage("Error: " + onError.toString());
+    })).user;
+    if (user != null) {
+      setState(() {
+        showSpinner=false;
+      });
+      Navigator.pushNamed(context, MainScreen.idScreen);
+    }
+
+    // final User userData = userCredential.user;
+    if (user != null) {
+      userRef.child(user.uid).once().then((value) => (DataSnapshot snap) {
             if (snap != null) {
               Navigator.pushNamedAndRemoveUntil(
                   context, MainScreen.idScreen, (route) => false);
               displayMessage('Your are logged in successfully');
             } else {
-              _firebaseAuth.signOut();
               displayMessage('No record found');
+              _firebaseAuth.signOut();
+
             }
           });
     }
@@ -45,97 +63,109 @@ class LoginScreen extends StatelessWidget {
     // TODO: implement build
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              SizedBox(height: 55.0),
-              Image(
-                image: AssetImage("images/logo.png"),
-                width: 200,
-                height: 100,
-                alignment: Alignment.center,
-              ),
-              SizedBox(height: 15.0),
-              Text(
-                "Login as a Rider",
-                style: TextStyle(fontSize: 24, fontFamily: "Brand-Bold"),
-                textAlign: TextAlign.center,
-              ),
-              Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Column(
-                  children: [
-                    SizedBox(height: 10.0),
-                    TextFormField(
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                SizedBox(height: 55.0),
+                Image(
+                  image: AssetImage("images/logo.png"),
+                  width: 200,
+                  height: 100,
+                  alignment: Alignment.center,
+                ),
+                SizedBox(height: 15.0),
+                Text(
+                  "Login as a Rider",
+                  style: TextStyle(fontSize: 24, fontFamily: "Brand-Bold"),
+                  textAlign: TextAlign.center,
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Column(
+                    children: [
+                      SizedBox(height: 10.0),
+                      TextFormField(
+                        onChanged: (value){
+                          emailController=value;
+                        },
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                            labelText: "Email",
+                            labelStyle: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 10.0,
+                                fontFamily: "Brand-Regular")),
+                      ),
+                      SizedBox(height: 10.0),
+                      TextFormField(
+                        obscureText: true,
+                        onChanged: (value){
+                          passwordController=value;
+                        },
+                        decoration: InputDecoration(
+                            labelText: "Password",
+                            labelStyle: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 10.0,
+                                fontFamily: "Brand-Regular")),
+                      ),
+                      SizedBox(height: 30.0),
+                      ElevatedButton(
+                        onPressed: () {
 
-                      onChanged: (value){
-                        emailController=value;
-                      },
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: InputDecoration(
-                          labelText: "Email",
-                          labelStyle: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 10.0,
-                              fontFamily: "Brand-Regular")),
-                    ),
-                    SizedBox(height: 10.0),
-                    TextFormField(
-                      obscureText: true,
-                      onChanged: (value){
-                        passwordController=value;
-                      },
-                      decoration: InputDecoration(
-                          labelText: "Password",
-                          labelStyle: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 10.0,
-                              fontFamily: "Brand-Regular")),
-                    ),
-                    SizedBox(height: 30.0),
-                    ElevatedButton(
-                      onPressed: () {
-                        loginAuthenticateUser(context);
-                      },
-                      onLongPress: () {},
-                      style: ElevatedButton.styleFrom(
-                          primary: Colors.yellow,
-                          onPrimary: Colors.white,
-                          shadowColor: Colors.red,
-                          elevation: 5.0,
-                          shape: BeveledRectangleBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(5)))),
-                      child: Container(
-                        height: 40.0,
-                        child: Center(
-                          child: Text(
-                            'Login',
-                            style:
-                                TextStyle(color: Colors.blue, fontSize: 24.0),
+                          if (emailController.isEmpty) {
+                            displayMessage('Enter valid email id');
+                          } else if (passwordController.isEmpty) {
+                            displayMessage('Mobile number is mandatory.');
+                          }
+                          else{
+                            setState(() {
+                              showSpinner=true;
+                            });
+                          loginAuthenticateUser(context);}
+                        },
+                        onLongPress: () {},
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.yellow,
+                            onPrimary: Colors.white,
+                            shadowColor: Colors.red,
+                            elevation: 5.0,
+                            shape: BeveledRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(5)))),
+                        child: Container(
+                          height: 40.0,
+                          child: Center(
+                            child: Text(
+                              'Login',
+                              style:
+                                  TextStyle(color: Colors.blue, fontSize: 24.0),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    SizedBox(height: 30.0),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => RegistrationScreen()));
-                      },
-                      child: Text(
-                        "Do not have an Account? Register here.",
-                        style: TextStyle(fontFamily: "Brand-Regular"),
-                      ),
-                    )
-                  ],
+                      SizedBox(height: 30.0),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => RegistrationScreen()));
+                        },
+                        child: Text(
+                          "Do not have an Account? Register here.",
+                          style: TextStyle(fontFamily: "Brand-Regular"),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:rider_app/main.dart';
 import 'package:rider_app/screens/main_screen.dart';
 
@@ -18,6 +19,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   String email;
   String password;
   String mobileNumber;
+  bool showSpinner=false;
 
   void registerUser(BuildContext context) async {
     final userCredential = await _firebaseAuth
@@ -30,10 +32,16 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
       //save data into firebase
       Map userMapData = {"name": name, "email": email, "phone": mobileNumber};
       userRef.child(userData.uid).set(userMapData);
+      setState(() {
+        showSpinner=false;
+      });
       displayMessage('Congratulations, Your account has been created');
       Navigator.pushNamedAndRemoveUntil(
           context, MainScreen.idScreen, (route) => false);
     } else {
+      setState(() {
+        showSpinner=false;
+      });
       //show error message
       displayMessage('User has not been created');
     }
@@ -47,129 +55,135 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              SizedBox(height: 55.0),
-              Image(
-                image: AssetImage("images/logo.png"),
-                width: 200,
-                height: 100,
-                alignment: Alignment.center,
-              ),
-              SizedBox(height: 15.0),
-              Text(
-                "Register as a Rider",
-                style: TextStyle(fontSize: 24, fontFamily: "Brand-Bold"),
-                textAlign: TextAlign.center,
-              ),
-              Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Column(
-                  children: [
-                    SizedBox(height: 10.0),
-                    TextFormField(
-                      keyboardType: TextInputType.text,
-                      onChanged: (value) {
-                        name = value;
-                      },
-                      decoration: InputDecoration(
-                          labelText: "Name",
-                          labelStyle: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 10.0,
-                              fontFamily: "Brand-Regular")),
-                    ),
-                    SizedBox(height: 10.0),
-                    TextFormField(
-                      keyboardType: TextInputType.emailAddress,
-                      onChanged: (value) {
-                        email = value;
-                      },
-                      decoration: InputDecoration(
-                          labelText: "Email",
-                          labelStyle: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 10.0,
-                              fontFamily: "Brand-Regular")),
-                    ),
-                    SizedBox(height: 10.0),
-                    TextFormField(
-                      obscureText: true,
-                      onChanged: (value) {
-                        password = value;
-                      },
-                      decoration: InputDecoration(
-                          labelText: "Password",
-                          labelStyle: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 10.0,
-                              fontFamily: "Brand-Regular")),
-                    ),
-                    SizedBox(height: 10.0),
-                    TextFormField(
-                      onChanged: (value) {
-                        mobileNumber = value;
-                      },
-                      keyboardType: TextInputType.phone,
-                      decoration: InputDecoration(
-                          labelText: "Phone",
-                          labelStyle: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 10.0,
-                              fontFamily: "Brand-Regular")),
-                    ),
-                    SizedBox(height: 30.0),
-                    ElevatedButton(
-                      onPressed: () {
-                        if (name.length > 4) {
-                          displayMessage('Name must be 3 character length');
-                        } else if (!email.contains("@")) {
-                          displayMessage('Enter valid email id');
-                        } else if (mobileNumber.isEmpty) {
-                          displayMessage('Mobile number is mandatory.');
-                        } else if (password.length < 5) {
-                          displayMessage('Password should be 6 character');
-                        } else {
-                          registerUser(context);
-                        }
-                      },
-                      onLongPress: () {},
-                      style: ElevatedButton.styleFrom(
-                          primary: Colors.yellow,
-                          onPrimary: Colors.white,
-                          shadowColor: Colors.red,
-                          elevation: 5.0,
-                          shape: BeveledRectangleBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(5)))),
-                      child: Container(
-                        height: 40.0,
-                        child: Center(
-                          child: Text(
-                            'Create an Account',
-                            style:
-                                TextStyle(color: Colors.blue, fontSize: 24.0),
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                SizedBox(height: 55.0),
+                Image(
+                  image: AssetImage("images/logo.png"),
+                  width: 200,
+                  height: 100,
+                  alignment: Alignment.center,
+                ),
+                SizedBox(height: 15.0),
+                Text(
+                  "Register as a Rider",
+                  style: TextStyle(fontSize: 24, fontFamily: "Brand-Bold"),
+                  textAlign: TextAlign.center,
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Column(
+                    children: [
+                      SizedBox(height: 10.0),
+                      TextFormField(
+                        keyboardType: TextInputType.text,
+                        onChanged: (value) {
+                          name = value;
+                        },
+                        decoration: InputDecoration(
+                            labelText: "Name",
+                            labelStyle: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 10.0,
+                                fontFamily: "Brand-Regular")),
+                      ),
+                      SizedBox(height: 10.0),
+                      TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        onChanged: (value) {
+                          email = value;
+                        },
+                        decoration: InputDecoration(
+                            labelText: "Email",
+                            labelStyle: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 10.0,
+                                fontFamily: "Brand-Regular")),
+                      ),
+                      SizedBox(height: 10.0),
+                      TextFormField(
+                        obscureText: true,
+                        onChanged: (value) {
+                          password = value;
+                        },
+                        decoration: InputDecoration(
+                            labelText: "Password",
+                            labelStyle: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 10.0,
+                                fontFamily: "Brand-Regular")),
+                      ),
+                      SizedBox(height: 10.0),
+                      TextFormField(
+                        onChanged: (value) {
+                          mobileNumber = value;
+                        },
+                        keyboardType: TextInputType.phone,
+                        decoration: InputDecoration(
+                            labelText: "Phone",
+                            labelStyle: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 10.0,
+                                fontFamily: "Brand-Regular")),
+                      ),
+                      SizedBox(height: 30.0),
+                      ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            showSpinner=true;
+                          });
+                          if (name.length < 3) {
+                            displayMessage('Name must be 3 character length');
+                          } else if (!email.contains("@")) {
+                            displayMessage('Enter valid email id');
+                          } else if (mobileNumber.isEmpty) {
+                            displayMessage('Mobile number is mandatory.');
+                          } else if (password.length < 5) {
+                            displayMessage('Password should be 6 character');
+                          } else {
+                            registerUser(context);
+                          }
+                        },
+                        onLongPress: () {},
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.yellow,
+                            onPrimary: Colors.white,
+                            shadowColor: Colors.red,
+                            elevation: 5.0,
+                            shape: BeveledRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(5)))),
+                        child: Container(
+                          height: 40.0,
+                          child: Center(
+                            child: Text(
+                              'Create an Account',
+                              style:
+                                  TextStyle(color: Colors.blue, fontSize: 24.0),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    SizedBox(height: 30.0),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: Text(
-                        "You have an already Account? Login here.",
-                        style: TextStyle(fontFamily: "Brand-Regular"),
-                      ),
-                    )
-                  ],
+                      SizedBox(height: 30.0),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text(
+                          "You have an already Account? Login here.",
+                          style: TextStyle(fontFamily: "Brand-Regular"),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
